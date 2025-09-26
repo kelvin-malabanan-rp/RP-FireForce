d5d3616192232087f3b57a3215fbd3fe82af8cfb
rp-keannubrillante <keannu.brillante@rocketpartners.io>
2025-09-26 23:03:14 +0800

    Updated web name

--- module-rp-fireforce-mobile/utils/backend-url.ts
+++ module-rp-fireforce-mobile/utils/backend-url.ts
@@ -1,2 +1,2 @@
-export const BASE_URL_DEV = "https://localhost:8787";
-// export const BASE_URL_DEV = "https://incident-webhook-api.rapidresponse.workers.dev";
+//export const BASE_URL_DEV = "https://localhost:8787";
+export const BASE_URL_DEV = "https://incident-webhook-api.rapidresponse.workers.dev";
